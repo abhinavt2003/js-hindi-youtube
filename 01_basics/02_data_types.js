077c5adbef9c283591f3c7b41e7b2bba6306af8d
Abhinav Tiwari <134359874+abhinavt2003@users.noreply.github.com>
2024-03-20 03:49:47 +0000

    data_types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as new js version
+
+//alert(3+3) // we are using node js not browser
+
+console.log(3
+    +3
+    ) ; console.log("abhinav") //Readability km ho jaegi //Code readability must be high
+
+let name="abhi"
+let age=18
+let isLoggedIn= false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true / false
+//null => standalone value / empty value
+//undefined => value defined nhi hui hai
+//symbol => uniqueness define krte hai (React me use hoga)
+
+//object
+
+console.log(typeof null)  //Null ka type object hai
+console.log(typeof undefined)  //undefined ka undefined hi hai
+console.log(typeof age)
+console.log(typeof name)
