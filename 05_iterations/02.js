643b6f197bdb0faabfce215ee3c054c21b3ab05c
abhinavt1606 <2021cs_abhinavtiwari_a@nie.ac.in>
2024-04-11 17:46:32 +0530

    loops

---
+++ 05_iterations/02.js
@@ -0,0 +1,20 @@
+//while loop
+let index=0;
+// while (index<=10) {
+//     console.log('value of index is', index);
+//     index++;
+// }
+let myArr= ["Abhinav","Abhi","nav"]
+let arr=0;
+// while(arr<myArr.length){
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr++;
+// }
+
+//do while loop
+
+let score= 1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
