5f996c946e09508bbb50e890d4d9bd24070955fd
abhinavt1606 <2021cs_abhinavtiwari_a@nie.ac.in>
2024-04-19 15:40:32 +0530

    project1solutioncode

---
+++ 07_projects/events.js
@@ -0,0 +1,13 @@
+//Events
+
+// In browser many events are there. Moving a cursor is an event, closing a tab is also an event 
+//For now we can take events in functions and can do some activities on it.
+//button.addEventListener //Ye jo bhi events hoga usse sunega like click,hover
+
+// buttons.forEach(function(button){
+//     console.log(button);
+//     button.addEventListener('click',function(e){
+//       console.log(e)  //Kon sa event hai ye btaega
+//       console.log(e.target) //e.target likhne se ye pta chlta hai ki ye event aa kha se rha hai
+//     }) 
+// })
