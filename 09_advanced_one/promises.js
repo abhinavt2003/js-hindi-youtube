55b17b670c58782b6748aaa58f050a8cc4a62696
abhinavt1606 <2021cs_abhinavtiwari_a@nie.ac.in>
2024-04-22 01:09:29 +0530

    promises

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -37,7 +37,7 @@ promise3.then(function(user){
 
 const promise4= new Promise(function(resolve,reject){
     setTimeout(function(){
-        let error= true;
+        let error= true  //true set kroge to catch block run krega
         if(!error){
             resolve({username: "Abhinav", password: "123"})
         }
@@ -48,8 +48,71 @@ const promise4= new Promise(function(resolve,reject){
     },2000)
 })
 
-const username= promise4.then((user) => {
-    console.log(user);
-    return user.username
+//Chaining concept
+//DataBase connection me ye syntax bhot kaam aayega
+promise4
+.then((user) => {
+    // console.log(user);
+    return user.username  //Ye then() ko return krega
 })
-console.log(username);
+.then((username) => {
+    // console.log(username);
+})
+.catch(function(error){
+    // console.log(error)
+})
+.finally(() =>{    //promise chahe resolve ya reject ho ye hmesha chlega
+    // console.log("Promise is either resolved or rejected");
+})
+
+//Async and Await syntax
+const promise5= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error= true  //true set kroge to catch block run krega
+        if(!error){
+            resolve({username: "JavaScript", password: "123"})
+        }
+        else{
+            //Rejection ki baat aa gyi 
+            reject(`ERROR: JS went wrong`)
+        }
+    },2000)
+});
+
+//error true set hai mtlb error aayegi ab agr error aayegi to problem aayegi kyunki async/await directly error handle nhi krta hai
+//iske lie alg se try/catch block lgana pdega
+async function consumeFromPromise5(){   //async aa gya to hm andr await use kr skte hai
+    try{
+        const response= await promise5 
+        // console.log(response);
+    }
+    catch(error){
+        // console.log(error)
+    }
+}
+consumeFromPromise5()
+
+
+// async function getAllUsers(){
+//     try{
+//         const response= await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data= await response.json()    //Data string me aa rha hai usko json me convert kr do  //json() ka response lene me bhi time lgta hai to hme json ko bhi await krna pdega
+//         console.log(data);
+//     } catch(error){
+//         console.log("E: ",error);
+//     }
+// }
+// getAllUsers()
+
+
+//Same upr wle kaam ko then/catch se kro
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+}) 
+.catch((error) => console.log(error))
+
+//Phli json ka data aa rha hai then async wgrh aa rha. Lekin upr ka code phle run krna tha
